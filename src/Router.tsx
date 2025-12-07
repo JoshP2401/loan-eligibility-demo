fb448c45babf264f9d742fa8b65f00f96a6ac44a
JoshP2401 <peacocke.jay@gmail.com>
2025-12-07 17:24:09 +0200

    Create Router; add standin Dashboard component; add reactQuery

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./screens/Dashboard";
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Dashboard />} />
+        </Routes>
+    );
+};
+
+export default Router;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <ChakraProvider value={defaultSystem} children={<App />} />
+        <BrowserRouter>
+            <ChakraProvider value={defaultSystem} children={<App />} />
+        </BrowserRouter>
     </StrictMode>
 );
