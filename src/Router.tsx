fe3769aad43175959704efed557d59659b5bf8fd
JoshP2401 <peacocke.jay@gmail.com>
2025-12-09 13:23:02 +0200

    Wrap with Dashboard container.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "./screens/Dashboard";
+import DashboardOverview from "./components/DashboardOverview";
+import DashboardContainer from "./screens/DashboardContainer";
 
 const Router = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Dashboard />} />
-        </Routes>
+        <DashboardContainer>
+            <Routes>
+                <Route path="/" element={<DashboardOverview />} />
+            </Routes>
+        </DashboardContainer>
     );
 };
 
